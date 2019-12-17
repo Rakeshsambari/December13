71d1d81b6762f37b991ff40e9199d571e00d26e9
RAKESH <perfengineerrakesh@gmail.com>
2019-12-17 09:25:03 +0530

    fourth commit

---
+++ MyJavaPrograms/MyJavaProgramsDec17/src/HelloWorld.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author RAKESH
+ *
+ */
+public class HelloWorld {
+
+	String str="Hello World!";
+	
+	public void printMethod() {
+	System.out.println(str);
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HelloWorld hw=new HelloWorld();
+		hw.printMethod();
+		System.out.println("created by Me");
+		
+
+	}
+
+}
---
+++ MyJavaPrograms/MyJavaProgramsDec17/src/OddorEven.java
@@ -0,0 +1,21 @@
+
+public class OddorEven {
+	int num=9;
+	public void oddEven() {
+		
+		if(num%2==0) {
+			System.out.println("given number is Even");
+		}
+		else {
+			System.out.println("given number is odd");
+		}
+		}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OddorEven oe=new OddorEven();
+		oe.oddEven();
+
+	}
+
+}
